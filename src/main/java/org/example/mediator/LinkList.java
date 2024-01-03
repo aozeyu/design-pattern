a6e025267ffd86c75f6a8889d699e387535bbbd3
aozeyu <3484766443@qq.com>
2024-01-03 15:49:51 +0800

    加锁式单例模式

---
+++ src/main/java/org/example/mediator/LinkList.java
@@ -0,0 +1,11 @@
+package org.example.mediator;
+
+import java.util.*;
+
+public class LinkList {
+  public static void main(String[] args) {
+    ArrayList<Object> objects = new ArrayList<>();
+    objects.trimToSize();
+    System.out.println(objects);
+  }
+}
