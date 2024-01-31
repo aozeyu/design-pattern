2e9b602eb1e881b3cbc803138678ec0d244a9cd2
aozeyu <3484766443@qq.com>
2024-01-31 22:30:15 +0800

    option

--- src/main/java/StreamTest2.java
+++ src/main/java/StreamTest2.java
@@ -3,6 +3,11 @@ import java.util.*;
 public class StreamTest2 {
   public static void main(String[] args) {
     List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
-    numbers.stream().map( n -> n * n ).forEach(System.out::println);
+    numbers.stream().map(n -> n * n).forEach(System.out::println);
+
+    List<Integer> someNumbers = Arrays.asList(1, 2, 3, 4, 5);
+    Optional<Integer> firstSquareDivisibleByThree = someNumbers.stream().map(n -> n * n).filter(n -> n % 3 == 0).findFirst();
+
+    System.out.println(firstSquareDivisibleByThree);
   }
 }
