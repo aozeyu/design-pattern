0d8caf7d1c33813b52da7af091db82a7a344a8a9
aozeyu <3484766443@qq.com>
2024-02-03 10:43:37 +0800

    test

--- src/main/java/org/example/interview/countWordsIteratively.java
+++ src/main/java/org/example/interview/countWordsIteratively.java
@@ -1,7 +1,19 @@
 package org.example.interview;
 
 public class countWordsIteratively {
-  public int countWordsIteratively2(String s) {
+
+
+
+
+  public static void main(String[] args) {
+    final String SENTENCE =
+        " Nel mezzo del cammin di nostra vita " +
+            "mi ritrovai in una selva oscura" +
+            " ché la dritta via era smarrita ";
+
+    System.out.println("Found " + countWordsIteratively2(SENTENCE) + " words");
+  }
+  public static int countWordsIteratively2(String s) {
     int count = 0;
     boolean lastSpace = true;
     for (char c : s.toCharArray()) {
