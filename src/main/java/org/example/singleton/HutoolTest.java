249b40e5ed6540f204b139a7d5ff960a233eff89
aozeyu <3484766443@qq.com>
2024-02-24 23:32:44 +0800

    增加hutool

---
+++ src/main/java/org/example/singleton/HutoolTest.java
@@ -0,0 +1,14 @@
+package org.example.singleton;
+
+
+import cn.hutool.core.date.*;
+
+import java.util.*;
+
+public class HutoolTest {
+  public static void main(String[] args) {
+    Date date = DateUtil.date();
+    System.out.println(date);
+    System.out.println(DateUtil.offset(new Date(), DateField.MONTH, 1));
+  }
+}
