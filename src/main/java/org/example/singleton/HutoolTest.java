0a9636ec833319480da880ac9df5d3ff4a12e8dd
aozeyu <3484766443@qq.com>
2024-02-24 23:43:46 +0800

    圣诞节

--- src/main/java/org/example/singleton/HutoolTest.java
+++ src/main/java/org/example/singleton/HutoolTest.java
@@ -2,6 +2,7 @@ package org.example.singleton;
 
 
 import cn.hutool.core.date.*;
+import cn.hutool.core.util.*;
 
 import java.util.*;
 
@@ -10,5 +11,7 @@ public class HutoolTest {
     Date date = DateUtil.date();
     System.out.println(date);
     System.out.println(DateUtil.offset(new Date(), DateField.MONTH, 1));
+
+    System.out.println(StrUtil.join(",", "a", "b", "c"));
   }
 }
