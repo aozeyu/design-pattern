90f99f3a108c209f3e787ccd286e5a8c790e39b0
aozeyu <3484766443@qq.com>
2024-04-13 09:37:44 +0800

    反射

---
+++ src/main/java/org/example/singleton/GetClass2.java
@@ -0,0 +1,9 @@
+package org.example.singleton;
+
+public class GetClass2 {
+  public static void main(String[] args) {
+    Class clazz = User.class;
+    System.out.println(clazz);
+    System.out.println(clazz.getName());
+  }
+}
