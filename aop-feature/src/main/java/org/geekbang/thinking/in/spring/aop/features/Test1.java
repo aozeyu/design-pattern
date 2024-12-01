a8c16f15d37713b534cfbf93ff947efeff7d428a
aozeyu <3484766443@qq.com>
2024-12-01 10:12:49 +0800

    原型模式

---
+++ aop-feature/src/main/java/org/geekbang/thinking/in/spring/aop/features/Test1.java
@@ -0,0 +1,13 @@
+package org.geekbang.thinking.in.spring.aop.features;
+
+import java.util.*;
+
+public class Test1 {
+  public static void main(String[] args) {
+    ArrayList<Integer> al = new ArrayList<Integer>();
+    al.add(100);
+    al.add(200);
+    al.add(300);
+    System.out.println(al);
+  }
+}
